452b14a57347767639204445593f272c267434bd
andreapintoc <77283293+andreapintoc@users.noreply.github.com>
2024-09-27 11:18:20 -0400

    Director.java

--- Apple_Vs_Dell/src/Clases/Director.java
+++ Apple_Vs_Dell/src/Clases/Director.java
@@ -1,6 +1,6 @@
 public class Director extends Thread {
     private Empresa empresa;
-    private int diasRestantes;
+    private int diasrestantes;
 
     public Director(Empresa empresa) {
         this.empresa = empresa;
