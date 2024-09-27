e3ee5ae202839fb3ce28e68b6e39446481eea878
andreapintoc <77283293+andreapintoc@users.noreply.github.com>
2024-09-27 11:16:27 -0400

    Clase ProjectManager

---
+++ Apple_Vs_Dell/src/Clases/ProjectManager.java
@@ -0,0 +1,7 @@
+public class ProjectManager extends Thread {
+    private Empresa empresa;
+    private int diasrestantes;
+
+    public ProjectManager(Empresa empresa) {
+        this.empresa = empresa;
+    }
