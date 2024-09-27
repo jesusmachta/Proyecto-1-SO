0b0f1c4b07aa5e14e58b3a8256e53839ca1ccbbd
andreapintoc <77283293+andreapintoc@users.noreply.github.com>
2024-09-27 09:12:43 -0400

    Creación de clase

--- Apple_Vs_Dell/src/Clases/ProductorFuenteAlimentacion.java
+++ Apple_Vs_Dell/src/Clases/ProductorFuenteAlimentacion.java
@@ -1,1 +1,15 @@
+public class ProductorFuenteAlimentacion extends Thread {
+    private int id;
+    private int capacidadAlmacen;
+    private int produccionDiaria;
+    private int ultimoNumeroCarnet;
+    private Almacen almacen;
 
+    public ProductorFuenteAlimentacion(int id, int capacidadAlmacen, int produccionDiaria, int ultimoNumeroCarnet, Almacen almacen) {
+        this.id = id;
+        this.capacidadAlmacen = capacidadAlmacen;
+        this.produccionDiaria = produccionDiaria;
+        this.ultimoNumeroCarnet = ultimoNumeroCarnet;
+        this.almacen = almacen;
+    }
+}
